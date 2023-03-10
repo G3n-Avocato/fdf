cf32da82e613bbd890c43307b2395e48cf3bfc98
Laura Massonet <lamasson@2C4.42angouleme.fr>
2023-03-10 19:27:02 +0100

    mlx comprehension

--- main.c
+++ main.c
@@ -6,21 +6,42 @@
 /*   By: lamasson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 12:56:51 by lamasson          #+#    #+#             */
-/*   Updated: 2023/03/08 17:43:38 by lamasson         ###   ########.fr       */
+/*   Updated: 2023/03/10 18:07:25 by lamasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-#include <stdio.h>
+typedef struct s_data {
+	void	*img;
+	char	*addr;
+	int	bits_per_pixel;
+	int	line_length;
+	int	endian;
+}t_data;
+
+void	ft_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*((unsigned int*)dst) = color;
+}
+
+
 int	main(void)
 {
-//	t_param	**tab;
+					//	t_param	**tab;
 	void	*mlx;
-//	void	*mlx_win;
+	void	*mlx_win;
+	t_data	data;
 
 	mlx = mlx_init();
-	mlx_new_window(mlx, 1920, 1080, "Hello");
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello");
+	data.img = mlx_new_image(mlx, 1920, 1080);
+	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
+	ft_mlx_pixel_put(&data, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, data.img, 0, 0);
 	mlx_loop(mlx);
 
 }
